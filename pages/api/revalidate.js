c35e5c196470abe400a4af0a600219c861a2c57a
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-06-04 17:30:31 +0100

    added api route

---
+++ pages/api/revalidate.js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: "invalid token" });
+  }
+
+  try {
+    await res.unstable_revalidate("/");
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send("Error revalidating");
+  }
+}
