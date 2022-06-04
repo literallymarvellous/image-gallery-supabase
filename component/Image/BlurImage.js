ab6d42c245b4c18a8c6ff9fd58734ba15eba559d
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-06-04 17:14:05 +0100

    first commit

---
+++ component/Image/BlurImage.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { images } from "../../next.config";
+
+function cn(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Images = ({ image }) => {
+  const [isLoading, setLoading] = useState(true);
+
+  return (
+    <a href={image.href} className="group">
+      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+        <Image
+          alt=""
+          src={image.imageSrc}
+          layout="responsive"
+          width={150}
+          height={150}
+          className={cn(
+            "group-hover:opacity-75 duration-700 ease-in-out",
+            isLoading
+              ? "grayscale blur-2xl scale-110"
+              : "grayscale-0 blur-0 scale-100"
+          )}
+          onLoadingComplete={() => setLoading(false)}
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
+    </a>
+  );
+};
+
+export default Images;
